dc5f330aabf63b9d676284c81f5b748ec013fed2
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2024-11-12 12:34:52 +0200

    Add changes to registration

--- src/pages/registration/RegistrationPage.jsx
+++ src/pages/registration/RegistrationPage.jsx
@@ -1,4 +1,4 @@
-import RegistrationForm from "../../components/registrationForm /registrationForm";
+import RegistrationForm from "../../components/registrationForm/RegistrationForm";
 
 const RegistrationPage = () => {
   return <RegistrationForm />;
