f511072c48dbf524c0e028633c5b591a5c309c73
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2024-11-13 11:19:28 +0200

    Change home page

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,8 +5,8 @@ const HomePage = () => {
     <div className={css.mainCont}>
       <p className={css.homeText}>
         Hi there! 😌 Welcome to the personal contacts website. Here you can keep
-        your contacts save, add new ones and delete ones you don't need anymore.
-        To use all the priveleges just join the comunity and we hope you will
+        your contacts safe, add new ones and delete ones you don't need anymore.
+        To use all the privileges just join the community and we hope you will
         enjoy it 💁‍♀️
       </p>
     </div>
